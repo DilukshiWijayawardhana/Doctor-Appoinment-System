221a570cdc542dec92a7ecc4d4581dfe4865a84f
Dilukshi Wijayawardhana <101212054+DilukshiWijayawardhana@users.noreply.github.com>
2023-03-07 22:18:04 +0530

    created web pages

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require("express")
-const colors = require("colors")
-const morgan = reqire("morgan")
-const dotenv = require("dotenv")
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+//config dotenv
+dotenv.config();
 
 //rest object
 const app = express();
@@ -11,16 +14,31 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.get("/",(req,res) => {
+app.get('/',(req,res) => {
   res.status(200).send({
     message: "server running",
 
   });
 });
-//port
+/*port
+const node_mode = process.env.NODE_MODE || 'development';
 const port = process.env.PORT || 8080;
 
 //listen port
-app.listen(port, ()) => {
-  
-}
+app.listen(port, () => {
+   console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+    .bgCyan.white
+    );
+});*/
+
+// set default value for NODE_MODE to 'development'
+const node_mode = process.env.NODE_MODE || 'development';
+const port = process.env.PORT || 8080;
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server Running in ${node_mode} Mode on port ${port}`.bgCyan.white);
+});
+
+
